2dec47b73c2a4bfb149cadf29f9892875751e037
rvema <ragha.vema@capitalone.com>
2020-04-28 19:19:55 +0530

    travis build configuration

--- src/devops/release.sh
+++ src/devops/release.sh
@@ -2,10 +2,10 @@
 
 cp src/devops/.travis.settings.xml $HOME/.m2/settings.xml
 
-openssl aes-256-cbc -K $encrypted_00b218189f3e_key -iv $encrypted_00b218189f3e_iv -in src/devops/keys.gpg.enc -out keys.gpg -d
+#openssl aes-256-cbc -K $encrypted_00b218189f3e_key -iv $encrypted_00b218189f3e_iv -in src/devops/keys.gpg.enc -out keys.gpg -d
 
-gpg --fast-import keys.gpg
+#gpg --fast-import keys.gpg
 
-shred keys.gpg
+#shred keys.gpg
 
-mvn deploy -q -P release
+#mvn deploy -q -P release
