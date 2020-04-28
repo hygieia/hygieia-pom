8f6bf22b3b10661bf470af7ead9c521b4b012b81
rvema <ragha.vema@capitalone.com>
2020-04-28 19:49:19 +0530

    added keys for maven deployment

--- src/devops/release.sh
+++ src/devops/release.sh
@@ -3,9 +3,10 @@
 cp src/devops/.travis.settings.xml $HOME/.m2/settings.xml
 
 #openssl aes-256-cbc -K $encrypted_00b218189f3e_key -iv $encrypted_00b218189f3e_iv -in src/devops/keys.gpg.enc -out keys.gpg -d
+openssl aes-256-cbc -K $encrypted_287726384bcd_key -iv $encrypted_287726384bcd_iv -in src/devops/keys.gpg.enc -out keys.gpg -d
 
-#gpg --fast-import keys.gpg
+gpg --fast-import keys.gpg
 
-#shred keys.gpg
+shred keys.gpg
 
-#mvn deploy -q -P release
+mvn deploy -q -P release
